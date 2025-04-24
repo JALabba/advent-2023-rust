6bdc257b938f1f42ded0fca779f0a345fb2ba83a
JALabba <johnalabba@gmail.com>
2025-04-24 12:24:37 +0200

    added lcm and gcm to lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,22 @@
 pub mod template;
 
 // Use this file to add helper functions and additional modules.
+
+pub fn lcm(a: usize, b: usize)-> usize {
+    a * b / gcd(a,b)
+}
+
+pub fn gcd(mut a: usize, mut b: usize) -> usize {
+    if a == b {
+        return a;
+    }
+    if b > a {
+        std::mem::swap(&mut b, &mut a);
+    }
+    while b> 0 {
+        let t = a;
+        a = b;
+        b = t % b;
+    }
+    a
+}
